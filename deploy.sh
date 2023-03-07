7ed556bb78f15ed0f391cf3c7144de856b8555ac
Toola <contact@toola.fr>
2023-03-07 13:55:10 +0000

    hide webflow badge

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-
 read -p "Quel est le nom du site webflow à déployer? " SITE
 
 URL=http://$SITE.webflow.io/
-echo $URL
-
-#read -p "Quel est le chemin du dossier en local où le charger? " PATH
-#cd $PATH
 
 wget $URL -r --page-requisites --html-extension --convert-links -xnH -e robots=off
 
